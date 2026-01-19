472bf9ab577e0eb05e893f987fd5695c856b3888
Mamun <beparimamun708@gmail.com>
2026-01-19 16:08:30 +0600

    Create superuser automatically

--- core/urls.py
+++ core/urls.py
@@ -34,3 +34,7 @@ urlpatterns = [
 if settings.DEBUG:
    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
    
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username= 'admin').exists():
+   User.objects.create_superuser('admin', 'beparimamun708@gmail.com', 'mamun123')
