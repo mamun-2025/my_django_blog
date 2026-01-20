96bca9614d871291623305b4f050be56999690ed
Mamun <beparimamun708@gmail.com>
2026-01-20 17:41:15 +0600

    Final Cloudinary setup with CloudinaryField and requirements

--- my_blog_app/models.py
+++ my_blog_app/models.py
@@ -4,6 +4,7 @@ from PIL import Image
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
    name = models.CharField(max_length=100)
@@ -26,7 +27,7 @@ class Post(models.Model):
    author = models.ForeignKey(User, on_delete=models.CASCADE)
    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='posts')
    body = models.TextField()
-   image = models.ImageField(null=True, blank=True, upload_to='blog_images/')
+   image = CloudinaryField('image', null=True, blank=True)
    created_at = models.DateTimeField(auto_now_add=True)
 
    def save(self, *args, **kwargs):
@@ -45,24 +46,11 @@ class Comment(models.Model):
 
 class Profile(models.Model):
    user = models.OneToOneField(User, on_delete=models.CASCADE)
-   image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+   image = CloudinaryField('image', default='default.jpg')
    bio = models.TextField(blank=True)
 
    def __str__(self):
       return f"{self.user.username} Profile"
 
    def save(self, *args, **kwargs):
-      super().save(*args, **kwargs) # প্রথমে ছবিটিকে সেভ করে নেবে
-
-      # শুধু তখনই ইমেজ ওপেন করবে যদি ফাইলটি হার্ডড্রাইভে থাকে
-      if self.image and os.path.exists(self.image.path):
-         img = Image.open(self.image.path) # ছবির লোকেশন খুঁজে ওপেন করবে
-
-         # যদি ছবি ৩০০ পিক্সেলের চেয়ে বড় হয়, তবে তাকে ছোট করবে
-         if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path) # একই জায়গায় ছোট ছবিটিকে রিপ্লেস করবে
-
-   
-      
+      super().save(*args, **kwargs) 
